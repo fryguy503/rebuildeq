f9ceb962837c567c176d352c8624a4182329d22c
Xackery Xtal <xackery@gmail.com>
2016-01-25 19:54:02 -0800

    Removed unused /img/ info

---
+++ deploy/web/application/classes/Controller/Web/Build.php
@@ -0,0 +1,16 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Web_Build extends Template_Web_Core {
+
+
+	public function before() {
+
+		parent::before();
+		$this->template->site->title = "Dashboard";
+	}
+
+	public function action_index() {
+
+	}
+
+}
