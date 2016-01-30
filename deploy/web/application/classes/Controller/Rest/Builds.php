135528f21813700571a5e8b7d5adfee0c739bdf8
Xackery <xackery@gmail.com>
2016-01-30 13:30:40 -0800

    more simple sanity checks on rest/builds

--- deploy/web/application/classes/Controller/Rest/Builds.php
+++ deploy/web/application/classes/Controller/Rest/Builds.php
@@ -9,9 +9,9 @@ class Controller_Rest_Builds extends Template_Rest_Core {
 		$build = $this->request->post('build');
 		$character = DB::select()->from('character_data')
 		->where('session', '=', $session)
+		->where('session_timeout', '>=', DB::expr('NOW()'))
 		->limit(1)
-		->as_object()->execute()->current();
-		print_r($character);
+		->as_object()->execute()->current();		
 		if (empty($session)) {
 			$this->rest->Message = "Missing POST session";
 			return;
@@ -20,6 +20,16 @@ class Controller_Rest_Builds extends Template_Rest_Core {
 			$this->rest->Message = "Missing POST build";
 			return;
 		}
+		if (empty($character)) {
+			$this->rest->Message = "Invalid Session";
+			return;
+		}
+		if (!$this->validate_build($character, $build)) {
+			$this->rest->Message = "Invalid Build";
+		}
+
+		$this->rest->Status = 1;
+		$this->rest->Message = "Success!";
 		return;
 	}
 
