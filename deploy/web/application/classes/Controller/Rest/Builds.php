2969c5fdcf6448b42f596a6903e9f2abbeb259ef
Xackery <xackery@gmail.com>
2016-01-30 12:58:06 -0800

    modified rest/builds

--- deploy/web/application/classes/Controller/Rest/Builds.php
+++ deploy/web/application/classes/Controller/Rest/Builds.php
@@ -2,12 +2,37 @@
 
 class Controller_Rest_Builds extends Template_Rest_Core {
 
+	public function action_index() {
+		die("index");
+	}
+	
 	public function action_update() {
 		$this->rest->session = $this->request->post('session');
+
+		$build = $this->request->post('build');
+		$character = DB::select()->from('character_data')
+		->where('session', '=', $session)
+		->limit(1)
+		->as_object()->execute()->current();
+		print_r($character);
 		if (empty($this->rest->session)) {
 			$this->rest->Message = "Missing POST session";
+			return;
+		}
+		if empty($build) {
+			$this->rest->Message = "Missing POST build";
+			return;
 		}
 		return;
 	}
 
+	public function validate_build($character, $build) {
+		if (empty($build)) {
+			return false;
+		}
+		if empty($character) {
+			return false;
+		}
+		return true;
+	}
 }
