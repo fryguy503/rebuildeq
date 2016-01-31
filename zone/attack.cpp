fdf71d7ed71e3def4d34f55708bfb427f7b2224c
Xackery <xackery@gmail.com>
2016-01-30 19:28:15 -0800

    Added Soul Link Message of bonus dmg

--- zone/attack.cpp
+++ zone/attack.cpp
@@ -3161,9 +3161,8 @@ void Mob::CommonDamage(Mob* attacker, int32 &damage, const uint16 spell_id, cons
 				//Shin: If a lifetap and SK and have points into Soul Link
 				if (attacker && attacker->IsClient()) { // && attacker->CastToClient()->GetBuildRank(SHADOWKNIGHT, RB_SK_SOULLINK) > 0) {
 					uint32 rank = attacker->CastToClient()->GetBuildRank(SHADOWKNIGHT, RB_SK_SOULLINK);
-					attacker->CastToClient()->Message(0, "%i", damage);
+					attacker->CastToClient()->Message(MT_NonMelee, "Soul Link %u added %i bonus damage.", rank, int32((float)damage * 0.04 * (float)rank));
 					damage += int32((float)damage * 0.04 * (float)rank);
-					attacker->CastToClient()->Message(0, "%i", damage);
 				}
 
 				int healed = damage;
