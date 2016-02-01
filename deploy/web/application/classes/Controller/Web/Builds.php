2219aff9d49f5cdc0f4d5879ad610a1b18204e0d
Xackery Xtal <xackery@gmail.com>
2016-02-01 10:06:06 -0800

    Fixed line returns on descriptions

--- deploy/web/application/classes/Controller/Web/Builds.php
+++ deploy/web/application/classes/Controller/Web/Builds.php
@@ -18,7 +18,7 @@ class Controller_Web_Builds extends Template_Web_Core {
 			case "bard":
 			$fullName = "Bard";
 			$this->template->monogram = 8;
-			$desc = "Bards sing songs. Their 3 styles focus on group, melee, or solo.";
+			$desc = "Bards sing songs to aid allies in battle.<br> Their 3 styles focus on group, melee, or solo.";
 			$styles[0] = "Virtuoso";
 			$styles[1] = "Swordsinger";
 			$styles[2] = "Maestro";
@@ -26,7 +26,7 @@ class Controller_Web_Builds extends Template_Web_Core {
 			case "cleric":
 			$fullName = "Cleric";
 			$this->template->monogram = 2;
-			$desc = "Clerics are classically known as the best healers in the game. Their 3 styles focus on melee, healing, or protection.";
+			$desc = "Clerics are classically known as the best healers in the game.<br> Their 3 styles focus on melee, healing, or protection.";
 			$styles[0] = "Templar";
 			$styles[1] = "Bishop";
 			$styles[2] = "Archon";
@@ -34,7 +34,7 @@ class Controller_Web_Builds extends Template_Web_Core {
 			case "druid":
 			$fullName = "Druid";
 			$this->template->monogram = 6;
-			$desc = "Druids are one with nature. Their 3 styles focus on either Crowd Control, Heals, or Offense.";
+			$desc = "Druids are a mix of healer and damage dealer.<br> Their 3 styles focus on either Crowd Control, Heals, or Offense.";
 			$styles[0] = "Preserver";
 			$styles[1] = "Warden";
 			$styles[2] = "Hierophant";
@@ -42,7 +42,7 @@ class Controller_Web_Builds extends Template_Web_Core {
 			case "enchanter":
 			$fullName = "Enchanter";
 			$this->template->monogram = 14;
-			$desc = "Enchanters have had a heavy modification on Rebuild EQ. Due to illusions no longer being widespread, Illusion Magic is now more offensive. Their builds focus on either debuffs, domination, or inspiration.";
+			$desc = "Enchanters have had a heavy modification on Rebuild EQ.<br> Due to illusions no longer being easily accessible, illusions are now more offensive. Their builds focus on either debuffs, domination, or inspiration.";
 			$styles[0] = "Illusionist";
 			$styles[1] = "Entrancer";
 			$styles[2] = "Beguiler";
@@ -50,7 +50,7 @@ class Controller_Web_Builds extends Template_Web_Core {
 			case "magician":
 			$fullName = "Magician";
 			$this->template->monogram = 13;
-			$desc = "Magicians are conjurers. Their 3 styles focus on either stronger pets, area of effect damage, or empowering their utility skills";
+			$desc = "Magicians are conjurers of creatures from other planes or offensive specialists.<br> Their 3 styles focus on either stronger pets, area of effect damage, or empowering their utility skills";
 			$styles[0] = "Conjurer";
 			$styles[1] = "Elementalist";
 			$styles[2] = "Geomancer";
@@ -58,7 +58,7 @@ class Controller_Web_Builds extends Template_Web_Core {
 			case "monk":
 			$fullName = "Monk";
 			$this->template->monogram = 7;
-			$desc = "Monks are disciplined fighters. Their 3 styles focus on either melee dps, supporting others with chakra abilities, or utility pulling.";
+			$desc = "Monks are disciplined fighters.<br> Their 3 styles focus on either melee dps, supporting others with chakra abilities, or utility pulling.";
 			$styles[0] = "Master";
 			$styles[1] = "Transcendant";
 			$styles[2] = "Ashenhand";
@@ -66,7 +66,7 @@ class Controller_Web_Builds extends Template_Web_Core {
 			case "necromancer":
 			$fullName = "Necromancer";
 			$this->template->monogram = 11;
-			$desc = "Necromancers are master of the dead. They have the best DoTs of any class with one focus. The other 2 focus on debuffs, and tanking.";
+			$desc = "Necromancers are master of the dead.<br> They have the best DoTs of any class with one focus. The other 2 focus on debuffs, and tanking.";
 			$styles[0] = "Heretic";
 			$styles[1] = "Lich";
 			$styles[2] = "Warlock";
@@ -74,7 +74,7 @@ class Controller_Web_Builds extends Template_Web_Core {
 			case "paladin":
 			$this->template->monogram = 3;
 			$fullName = "Paladin";
-			$desc = "Paladins are the holy knights. Their 3 styles focus on better healing and support, tanking, or damage dealing.";
+			$desc = "Paladins are the holy knights.<br> Their 3 styles focus on better healing and support, tanking, or damage dealing.";
 			$styles[0] = "Protector";
 			$styles[1] = "Knight";
 			$styles[2] = "Crusader";
@@ -82,7 +82,7 @@ class Controller_Web_Builds extends Template_Web_Core {
 			case "ranger":
 			$fullName = "Ranger";
 			$this->template->monogram = 4;
-			$desc = "Rangers are friends with beasts. Their 3 styles focus on either better archery, enabling and strengthening a beast companion, or supporting others.";
+			$desc = "Rangers are friends with beasts.<br> Their 3 styles focus on either better archery, enabling and strengthening a beast companion, or supporting others.";
 			$styles[0] = "Archer";
 			$styles[1] = "Hunter";
 			$styles[2] = "Plainswalker";
@@ -90,7 +90,7 @@ class Controller_Web_Builds extends Template_Web_Core {
 			case "rogue":
 			$fullName = "Rogue";
 			$this->template->monogram = 9;
-			$desc = "Rogues are thieves. Their 3 styles focus on dealing damage, being an evasive tank, or stealing.";
+			$desc = "Rogues are thieves who specialize in deception.<br> Their 3 styles focus on dealing damage, being an evasive tank, or stealing.";
 			$styles[0] = "Assassin";
 			$styles[1] = "Deceiver";
 			$styles[2] = "Bandit";
@@ -99,7 +99,7 @@ class Controller_Web_Builds extends Template_Web_Core {
 			case "shaman":
 			$fullName = "Shaman";
 			$this->template->monogram = 10;
-			$desc = "Shamans are one with spirits. Their 3 styles focus on giving buffs among other utility bonuses, being a focused healer, or enchanting themselves to be more viable in melee.";
+			$desc = "Shamans are one with spirits, specializing in buffing.<br> Their 3 styles focus on giving buffs among other utility bonuses, being a focused healer, or enchanting themselves to be more viable in melee.";
 			$styles[0] = "Prophet";
 			$styles[1] = "Oracle";
 			$styles[2] = "Elder";
@@ -107,7 +107,7 @@ class Controller_Web_Builds extends Template_Web_Core {
 			case "warrior":
 			$fullName = "Warrior";
 			$this->template->monogram = 1;
-			$desc = "Warriors are hearty duelists. Gladiators will hone their blade mastery to allow them to deal damage unheard of before, Brawlers live off the thrill of battle, and Champions seek glory with their ability to survive.";
+			$desc = "Warriors are hearty duelists.<br> Gladiators will hone their blade mastery to allow them to deal damage unheard of before, Brawlers live off the thrill of battle, and Champions seek glory with their ability to survive.";
 			$styles[0] = "Gladiator";
 			$styles[1] = "Brawler";
 			$styles[2] = "Champion";
@@ -115,7 +115,7 @@ class Controller_Web_Builds extends Template_Web_Core {
 			case "wizard":
 			$fullName = "Wizard";
 			$this->template->monogram = 12;
-			$desc = "Wizards are masters of the elements. Evokers deal high elemental damage. Channelers focus on utility, while Sage is focused on support.";
+			$desc = "Wizards are masters of the elements.<br> Evokers deal high elemental damage. Channelers focus on utility, while Sage is focused on support.";
 			$styles[0] = "Evoker";
 			$styles[1] = "Channeler";
 			$styles[2] = "Sage";
@@ -124,7 +124,7 @@ class Controller_Web_Builds extends Template_Web_Core {
 			default:
 			$fullName = "Shadow Knight";
 			$this->template->monogram = 5;
-			$desc = "Shadow Knights are unholy knights. Their 3 styles focus tanking by taking blood of their victims, dealing damage with spears and other abilities, or crowd control pulling.";
+			$desc = "Shadow Knights are unholy knights.<br> Their 3 styles focus tanking by taking blood of their victims, dealing damage with spears and other abilities, or crowd control pulling.";
 			$styles[0] = "Bloodreaver";
 			$styles[1] = "Defiler";
 			$styles[2] = "Revenant";
