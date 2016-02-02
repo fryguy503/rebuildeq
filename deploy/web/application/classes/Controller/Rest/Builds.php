9173864de094094bb72c67aa33b539d886ada12a
Xackery <xackery@gmail.com>
2016-02-01 19:25:34 -0800

    Added build increment request system

--- deploy/web/application/classes/Controller/Rest/Builds.php
+++ deploy/web/application/classes/Controller/Rest/Builds.php
@@ -3,32 +3,51 @@
 class Controller_Rest_Builds extends Template_Rest_Core {
 
 	public function action_update() {
+		$buildIndex = $this->request->post('buildindex');
+		if (empty($buildIndex) || intval($buildIndex) > 53 || intval($buildIndex) < 1) {
+			$this->rest->Message = "Invalid buildIndex: ".intval($buildIndex);
+			return;
+		}
 
 		$session = $this->request->post('session');
 
-		$build = $this->request->post('build');
 		$character = DB::select()->from('character_data')
 		->where('session', '=', $session)
 		->where('session_timeout', '>=', DB::expr('NOW()'))
 		->limit(1)
-		->as_object()->execute()->current();		
+		->as_object()->execute()->current();
+
 		if (empty($session)) {
 			$this->rest->Message = "Missing POST session";
-			return;
-		}
-		if (empty($build)) {
-			$this->rest->Message = "Missing POST build";
 			return;
 		}
 		if (empty($character)) {
 			$this->rest->Message = "Invalid Session";
 			return;
 		}
-		if (!$this->validate_build($character, $build)) {
-			$this->rest->Message = "Invalid Build";
+		$build = $character->build_data;
+		$build = Build::clean($build);
+		if (!Build::validate($build)) {
+			$this->rest->Message = "Invalid Build Before Set";
+			return;
+		}
+		$newBuild = $build;
+		$newBuild[$buildIndex] = intval($newBuild[$buildIndex]) + 1;
+		$newBuild = Build::clean($newBuild);
+		if ($newBuild == $build) {
+			$this->rest->Message = "Changes discarded, old and new builds are same.";
+			return;
+		}
+		if (!Build::validate($newBuild)) {
+			$this->rest->Message = "Invalid build request sent";
+			return;
 		}
 
+		$data = array('build_data' => $newBuild);
+		$query = DB::update('character_data')->set($data)->where('session', "=", $session)->limit(1)->execute();
 		$this->rest->Status = 1;
+		$this->rest->oldBuild = $build;
+		$this->rest->newBuild = $newBuild;
 		$this->rest->Message = "Success!";
 		return;
 	}
