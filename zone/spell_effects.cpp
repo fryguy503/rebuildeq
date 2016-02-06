cc3a7fd44f8174d03eba1ebad62d472f42687ee2
Xackery Xtal <xackery@gmail.com>
2016-02-05 16:45:16 -0800

    Festering Spear now went from 10% to 20% damage per rank.

--- zone/spell_effects.cpp
+++ zone/spell_effects.cpp
@@ -239,7 +239,7 @@ bool Mob::SpellEffect(Mob* caster, uint16 spell_id, float partial, int level_ove
 						if (caster && caster->IsClient() && caster->CastToClient()->GetBuildRank(SHADOWKNIGHT, RB_SK_FESTERINGSPEAR) > 0) {
 							uint16 rank = caster->CastToClient()->GetBuildRank(SHADOWKNIGHT, RB_SK_FESTERINGSPEAR);
 							if (spell_id == 5012 || spell_id == 3561 || spell_id == 3560 || spell_id == 3562) { //spear spells
-								int festerDmg = int32((float)dmg * 0.1 * (float)rank);
+								int festerDmg = int32((float)dmg * 0.2 * (float)rank);
 								caster->CastToClient()->Message(MT_NonMelee, "Festering Spear %u added %i bonus damage.", rank, festerDmg);
 								dmg += festerDmg;
 							}
