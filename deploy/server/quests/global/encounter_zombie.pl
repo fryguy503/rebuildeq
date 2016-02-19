bedd3451fa10516609737b45bd1a55464433f757
Xackery <xackery@gmail.com>
2016-02-18 20:11:44 -0800

    Encounter Zombie now cleaned up

--- deploy/server/quests/global/encounter_zombie.pl
+++ deploy/server/quests/global/encounter_zombie.pl
@@ -1,11 +1,18 @@
 #encounter_zombie 187000
 
 my @group;
+my @groupname;
 my @enemies;
 my $isEventFinished = 0;
 my $waveCount = 0;
 
-sub EVENT_SPAWN {	
+
+sub EVENT_SPAWN {
+	@group = ();
+	@groupname = ();
+	@enemies = ();
+	$isEventFinished = 0;
+	$waveCount = 0;
 	quest::settimer("start", 1);
 	quest::settimer("eventendcheck", 3)
 }
@@ -14,29 +21,44 @@ sub EVENT_SPAWN {
 sub EVENT_TIMER {
 
 	if ($timer eq "eventendcheck") { #See if event is ending
+		#quest::say("Finishing ".$groupname[0]);
 		#is event finished?
 		if (!$isEventFinished) { return; }
 		#see if all enemies are dead.
 		foreach $z (@enemies) {
-			if ($z && !$z->IsDead()) { next; }
+			$checkMob = $entity_list->GetMobID($newid);	
+			if ($checkMob && !$checkMob->IsCorpse()) { return; }
 		}
-		#give out reward.
 
-		#Bye!
+		$winnerList = "";
+    	$dbh = plugin::LoadMysql();
+		for ($i = 0; $i < 6; $i++) {
+			my $char_id = $npc->GetEntityVariable("member$i");
+			if ($char_id < 1) { next; }
+	    	$sth = $dbh->prepare("UPDATE `character_custom` SET unclaimed_encounter_rewards = unclaimed_encounter_rewards + 1 WHERE character_id = ?");
+	    	$sth->execute($char_id);
+	    	$winnerList .= $groupname[$i].", ";
+	    }
+	    $winnerList = substr $winnerList, 0, -2;
+	    quest::we(13, "$winnerList successfully stopped a zombie invasion in $zoneln!");
+    	$dbh->disconnect();
+    	#despawn
 		quest::depop();
 	}
 
 	if ($timer eq "start") { #encounter init, set variables to an easier to handle group
+
 		quest::stoptimer("start");
 		#store group details
 		for ($i = 0; $i < 6; $i++) {
 			my $groupsize = scalar @group;
-			quest::say($i);
 			$c = $entity_list->GetClientByCharID($npc->GetEntityVariable("member$i"));			
 			if ($c) {
 				$group[$groupsize] = $c;
+				$groupname[$groupsize] = $c->GetCleanName();
 			}
 		}
+		#quest::say("Setting ".$groupname[0]);
 		#spawn first wave of enemies
 		foreach $c (@group) {
 			if (!$c) { next; }
@@ -53,23 +75,28 @@ sub EVENT_TIMER {
 
 			#these should move to mob
 			$newnpc->SpellEffect(13); #Earth rising up effect
-			$newnpc->Stun(1);
+			$newnpc->Stun(1000);
 			#$newspawn->DoAnim(110); #sit
 			
 			#prep mob and add to local tracker
-			$zindex = scalar @enemies;	
+			$zindex = scalar @enemies;
 			$newnpc->AddToHateList($c, 1);
-			$enemies[$zindex] = $newspawn;
-			quest::settimer("nexthorde", quest::ChooseRandom(1,3,6,9,12,15,20));
+			$enemies[$zindex] = $newmob->GetID();
+			quest::settimer("nexthorde", 1);			
+			#quest::settimer("nexthorde", quest::ChooseRandom(1,3,6,9,12,15,20));
+			#quest::say("Done");
 		}
 		$waveCount = 1;
 		return;
 	}
 
 	if ($timer eq "nexthorde") {
-		quest::stoptimer("nexthode");
+		quest::stoptimer("nexthorde");		
 		$waveCount++;
-		if (int(rand(5 * waveCount)) > 5) { #wave2 50%, wave3 33%, wave4 25%, etc
+		#quest::say($waveCount);
+		my $dice = int(rand(5 * $waveCount));
+		#quest::say($dice);
+		if ($dice >= 5) { #wave2 50%, wave3 33%, wave4 25%, etc
 			$isEventFinished = 1;
 			return;
 		}
@@ -79,7 +106,8 @@ sub EVENT_TIMER {
 		#TODO Add enemy
 
 		#make another wave happen, potentially.
-		quest::settimer("nexthorde", quest::ChooseRandom(1,3,6,9,12,15,20));
+		$nextDelay = quest::ChooseRandom(1,3,6,9,12,15,20);
+		quest::settimer("nexthorde", ($nextDelay + $waveCount));
 	}	
 
 }
