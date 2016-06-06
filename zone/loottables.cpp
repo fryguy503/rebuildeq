de1b2408ed8f57436d248efcc8be2957eba728ab
Xackery <xackery@gmail.com>
2016-06-06 00:07:20 -0700

    Added shadowed man card.

--- zone/loottables.cpp
+++ zone/loottables.cpp
@@ -484,6 +484,8 @@ void NPC::AddCardTable() {
 	if (GetRace() == HALFLING) { pool + 5; cardTable[pool] = 100131; } //Halfling Card
 	if (GetRace() == GNOME) { pool + 5; cardTable[pool] = 100132; } //Gnome Card
 	if (GetRace() == FROGLOK || GetRace() == FROGLOK2) { pool + 5; cardTable[pool] = 100133; } //Froglok Card
+	if (GetRace() == INVISIBLE_MAN ) { pool + 10; cardTable[pool] = 100134; } //Shadowed Man Card
+
 
 	if (pool < 1) return;
 
