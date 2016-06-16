849cee53f32b6285e299405a9154365a1744f5b8
Xackery <xackery@gmail.com>
2016-06-16 00:38:26 -0700

    added druid build icons

--- deploy/web/application/classes/Build.php
+++ deploy/web/application/classes/Build.php
@@ -499,100 +499,100 @@ class Build {
 			$newSkill = new stdClass();
 			$newSkill->title = "Regeneration";
 			$newSkill->desc = 'Grants the druid an innate hp regeneration of Level * 0.2 hitpoints (minimum 2) per tick, as well as any duration regeneration spells casted by the druid give <em data-base="20">20</em>% additional hitpoint regeneration per tick while in the same zone as the druid<span class="perLevel"> per rank</span>.';
-			$newSkill->image = "/images/builds/icons/gunzerker-gunlust-0.png";	
+			$newSkill->image = "/images/builds/icons/rb-dru-regeneration.png";	
 			$skills[18] = $newSkill;
 
 			$newSkill = new stdClass();
 			$newSkill->title = "Convergence of Spirits";
 			$newSkill->desc = 'Grants the Convergence of Spirits AA, giving a heal over time that heals for <em data-base="0.5">0.5</em>% of the target\'s max health per tick for <em data-base="6">6</em> seconds<span class="perLevel"> per rank</span>.';
-			$newSkill->image = "/images/builds/icons/gunzerker-gunlust-0.png";	
+			$newSkill->image = "/images/builds/icons/rb-dru=convergenceofspirits..png";	
 			$skills[20] = $newSkill;
 
 			$newSkill = new stdClass();
 			$newSkill->title = "Lifeflow";
 			$newSkill->desc = 'While an ally has Covergence of Spirits or Nature\'s Boon buff on them, they are granted a <em data-base="1">1</em>% death save chance, healing when triggered for 8000 hitpoints<span class="perLevel"> per rank</span>.';
-			$newSkill->image = "/images/builds/icons/gunzerker-gunlust-0.png";	
+			$newSkill->image = "/images/builds/icons/rb-dru=lifeflow.png";	
 			$skills[22] = $newSkill;
 
 			$newSkill = new stdClass();
 			$newSkill->title = "Spirit of the Wood";
 			$newSkill->desc = 'Grants the Spirit of the Wood AA, which casts buffs scaled based on your level.<br>Rank 1: Strength<br>Rank 2: Resists<br>Rank 3: HP Regen<br>Rank 4: HP (Wood Line)<br>Rank 5: Damage Shield';
-			$newSkill->image = "/images/builds/icons/gunzerker-gunlust-0.png";	
+			$newSkill->image = "/images/builds/icons/rb-dru-spiritofthewood.png";	
 			$skills[24] = $newSkill;			
 
 			
 			$newSkill = new stdClass();
-			$newSkill->title = "Spirital Awakening";
+			$newSkill->title = "Spiritual Awakening";
 			$newSkill->desc = 'Any successful melee hit on an enemy by the druid grants <em data-base="1">1</em> mana<span class="perLevel"> per rank</span>.';
-			$newSkill->image = "/images/builds/icons/gunzerker-gunlust-0.png";
+			$newSkill->image = "/images/builds/icons/rb-dru-spiritualawakening.png";
 			$skills[26] = $newSkill;
 
 			$newSkill = new stdClass();
 			$newSkill->title = "Nature's Boon";
 			$newSkill->desc = 'Grants the Nature\'s Boon AA, where the target receives a buff that lasts 84 seconds, giving the target (Level*2)*0.2*rank hitpoints per tick. Each rank reduces the duration by <em data-base="12">12</em> seconds. When the buff fades, the target is healed for <em data-base="20">20</em>% of their max hitpoints<span class="perLevel"> per rank</span>.';
-			$newSkill->image = "/images/builds/icons/gunzerker-gunlust-0.png";	
+			$newSkill->image = "/images/builds/icons/rb-dru-naturesboon.png";	
 			$skills[28] = $newSkill;
 
 			$newSkill = new stdClass();
 			$newSkill->title = "Nature's Whisper";
 			$newSkill->desc = 'While a target is affected by Nature\'s Boon or Convergence of Spirits, they also regain <em data-base="2">2</em>% of any healing done from these spells as mana.<span class="perLevel"> per rank</span>.';
-			$newSkill->image = "/images/builds/icons/gunzerker-gunlust-0.png";	
+			$newSkill->image = "/images/builds/icons/rb-dru-natureswhisper.png";	
 			$skills[31] = $newSkill;
 
 			$newSkill = new stdClass();
 			$newSkill->title = "Nature's Guardian";
 			$newSkill->desc = 'Grants the Nature\'s Guardian AA, where <em data-base="3">3</em>% of the caster\'s current mana is infused by nature and can be transferred to a target or to the caster<span class="perLevel"> per rank</span>.';
-			$newSkill->image = "/images/builds/icons/gunzerker-gunlust-0.png";	
+			$newSkill->image = "/images/builds/icons/rb-dru-naturesguardian.png";	
 			$skills[34] = $newSkill;
 
 
 			$newSkill = new stdClass();
 			$newSkill->title = "Intensity";
 			$newSkill->desc = 'When an enemy is affected by a damage over time, there is a <em data-base="1">1</em>% chance the DoT will do triple damage on a single tick.';
-			$newSkill->image = "/images/builds/icons/gunzerker-gunlust-0.png";	
+			$newSkill->image = "/images/builds/icons/rb-dru-intensity.png";	
 			$skills[36] = $newSkill;
 
 			$newSkill = new stdClass();
 			$newSkill->title = "Lingering Pain";
 			$newSkill->desc = 'When a direct spell is casted, there is a <em data-base="3">3</em>% chance that a short duration DoT will also affect the target if the spell casted was obtained within 15 levels of the enemy\'s level<span class="perLevel"> per rank</span>.';
-			$newSkill->image = "/images/builds/icons/gunzerker-gunlust-0.png";	
+			$newSkill->image = "/images/builds/icons/rb-dru-lingeringpain.png";	
 			$skills[38] = $newSkill;
 
 			$newSkill = new stdClass();
 			$newSkill->title = "Focused Swarm";
 			$newSkill->desc = 'When standing nearby a mob, DoTs now deal <em data-base="10">10</em>% more damage. This damage bonus reduces by 10% for every 50m you\'re away from an enemy.<span class="perLevel"> per rank</span>.';
-			$newSkill->image = "/images/builds/icons/gunzerker-gunlust-0.png";	
+			$newSkill->image = "/images/builds/icons/rb-dru-focusedswarm.png";	
 			$skills[40] = $newSkill;
 
 			$newSkill = new stdClass();
 			$newSkill->title = "Blessing of Ro";
 			$newSkill->desc = 'All Ro debuffs now have a <em data-base="5">5</em>% chance to spread to enemies within <em data-base="10">10</em>m of an affected target if they are on the aggro list of the druid.';
-			$newSkill->image = "/images/builds/icons/gunzerker-gunlust-0.png";	
+			$newSkill->image = "/images/builds/icons/rb-dru-blessingofro.png";	
 			$skills[42] = $newSkill;
 
 
 			$newSkill = new stdClass();
 			$newSkill->title = "Whirling Disaster";
 			$newSkill->desc = 'Direct Damage spells now also have a <em data-base="2">2</em>% chance to stun a target for 2 seconds if the spell casted was obtained within 15 levels of the enemy\'s level<span class="perLevel"> per rank</span>.';
-			$newSkill->image = "/images/builds/icons/gunzerker-gunlust-0.png";	
+			$newSkill->image = "/images/builds/icons/rb-dru-whirlingdisaster.png";	
 			$skills[44] = $newSkill;
 
 			$newSkill = new stdClass();
 			$newSkill->title = "Stinging Affliction";
 			$newSkill->desc = 'DoTs and Direct Damage spells have a <em data-base="1"></em>% chance to critical hit<span class="perLevel"> per rank</span>.';
-			$newSkill->image = "/images/builds/icons/gunzerker-gunlust-0.png";	
+			$newSkill->image = "/images/builds/icons/rb-dru-stingingaffliction.png";	
 			$skills[46] = $newSkill;
 
 			$newSkill = new stdClass();
 			$newSkill->title = "Deep Roots";
 			$newSkill->desc = 'When a root wears off, there is a <em data-base="3"></em>% chance the target will be snared if it is not already.<span class="perLevel"> per rank</span>.';
-			$newSkill->image = "/images/builds/icons/gunzerker-gunlust-0.png";	
+			$newSkill->image = "/images/builds/icons/rb-dru-deeproots.png";	
 			$skills[49] = $newSkill;
 
 			$newSkill = new stdClass();
 			$newSkill->title = "Nature's Blight";
 			$newSkill->desc = 'Grants the Lesson of the Devoted AA, causing the target to take 5% increased spell damage for <em data-base="24">24</em> seconds <span class="perLevel"> per rank</span>.';
-			$newSkill->image = "/images/builds/icons/gunzerker-gunlust-0.png";	
+			$newSkill->image = "/images/builds/icons/rb-dru-naturesblight.png";	
 			$skills[52] = $newSkill;
 			break;
 			
