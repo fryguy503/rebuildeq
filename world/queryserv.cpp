3357784b84e7d8ed912375c8b53ba331ceda9f17
Xackery <xackery@gmail.com>
2016-06-17 23:16:32 -0700

    added channelmessage to queryserv.

--- world/queryserv.cpp
+++ world/queryserv.cpp
@@ -4,7 +4,10 @@
 #include "world_config.h"
 #include "clientlist.h"
 #include "zonelist.h"
-
+#include "cliententry.h"
+#include "console.h"
+#include "ucs.h"
+#include "zoneserver.h"
 
 #include "../common/md5.h"
 #include "../common/emu_tcp_connection.h"
@@ -12,6 +15,11 @@
 
 extern ClientList client_list;
 extern ZSList zoneserver_list;
+
+extern ConsoleList console_list;
+extern volatile bool RunLoops;
+extern UCSConnection UCSLink;
+extern QueryServConnection QSLink;
 
 QueryServConnection::QueryServConnection()
 {
@@ -110,6 +118,76 @@ bool QueryServConnection::Process()
 			case ServerOP_LFGuildUpdate:
 			{
 				zoneserver_list.SendPacket(pack);
+				break;
+			}
+			case ServerOP_ChannelMessage: {
+				if (pack->size < sizeof(ServerChannelMessage_Struct))
+					break;
+				ServerChannelMessage_Struct* scm = (ServerChannelMessage_Struct*)pack->pBuffer;
+				
+				if (scm->chan_num == 7 || scm->chan_num == 14) {
+					if (scm->deliverto[0] == '*') {
+						Console* con = 0;
+						con = console_list.FindByAccountName(&scm->deliverto[1]);
+						if (((!con) || (!con->SendChannelMessage(scm))) && (!scm->noreply))
+							zoneserver_list.SendEmoteMessage(scm->from, 0, 0, 0, "%s is not online at this time.", scm->to);
+						break;
+					}
+					ClientListEntry* cle = client_list.FindCharacter(scm->deliverto);
+					if (cle == 0 || cle->Online() < CLE_Status_Zoning ||
+						(cle->TellsOff() && ((cle->Anon() == 1 && scm->fromadmin < cle->Admin()) || scm->fromadmin < 80))) {
+						if (!scm->noreply) {
+							ClientListEntry* sender = client_list.FindCharacter(scm->from);
+							if (!sender || !sender->Server())
+								break;
+							scm->noreply = true;
+							scm->queued = 3; // offline
+							strcpy(scm->deliverto, scm->from);
+							// ideally this would be trimming off the message too, oh well
+							sender->Server()->SendPacket(pack);
+						}
+					}
+					else if (cle->Online() == CLE_Status_Zoning) {
+						if (!scm->noreply) {
+							ClientListEntry* sender = client_list.FindCharacter(scm->from);
+							if (cle->TellQueueFull()) {
+								if (!sender || !sender->Server())
+									break;
+								scm->noreply = true;
+								scm->queued = 2; // queue full
+								strcpy(scm->deliverto, scm->from);
+								sender->Server()->SendPacket(pack);
+							}
+							else {
+								size_t struct_size = sizeof(ServerChannelMessage_Struct) + strlen(scm->message) + 1;
+								ServerChannelMessage_Struct *temp = (ServerChannelMessage_Struct *) new uchar[struct_size];
+								memset(temp, 0, struct_size); // just in case, was seeing some corrupt messages, but it shouldn't happen
+								memcpy(temp, scm, struct_size);
+								temp->noreply = true;
+								cle->PushToTellQueue(temp); // deallocation is handled in processing or deconstructor
+
+								if (!sender || !sender->Server())
+									break;
+								scm->noreply = true;
+								scm->queued = 1; // queued
+								strcpy(scm->deliverto, scm->from);
+								sender->Server()->SendPacket(pack);
+							}
+						}
+					}
+					else if (cle->Server() == 0) {
+						if (!scm->noreply)
+							zoneserver_list.SendEmoteMessage(scm->from, 0, 0, 0, "You told %s, '%s is not contactable at this time'", scm->to, scm->to);
+					}
+					else
+						cle->Server()->SendPacket(pack);
+				}
+				else {
+					if (scm->chan_num == 5 || scm->chan_num == 6 || scm->chan_num == 11) {
+						console_list.SendChannelMessage(scm);
+					}
+					zoneserver_list.SendPacket(pack);
+				}
 				break;
 			}
 			default:
