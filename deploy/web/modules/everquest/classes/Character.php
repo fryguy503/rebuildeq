9fe45d8a2774d0e1e543436156766f7cc6c49003
Xackery Xtal <xackery@gmail.com>
2016-06-24 16:05:18 -0700

    fixed how a query looks

--- deploy/web/modules/everquest/classes/Character.php
+++ deploy/web/modules/everquest/classes/Character.php
@@ -4,7 +4,7 @@ class Character {
 
 	public static function count() {
  		$count = DB::select('count(last_login)')->from('character_data')
-		->where('last_login', '<=', DB::expr('UNIX_TIMESTAMP(NOW())-600'))
+		->where('last_login', '>=', DB::expr('UNIX_TIMESTAMP(NOW())-600'))
 		->execute()->current();
 		if (empty($count)) return 0;
 		return intval($count['count(last_login)']);
