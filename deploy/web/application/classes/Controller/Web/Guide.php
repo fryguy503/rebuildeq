e254b4eed2134aeb37fcdefd079b6026e696fb72
Xackery Xtal <xackery@gmail.com>
2016-09-15 12:26:49 -0700

    Fixed opengraph data for classes.

--- deploy/web/application/classes/Controller/Web/Guide.php
+++ deploy/web/application/classes/Controller/Web/Guide.php
@@ -27,6 +27,9 @@ class Controller_Web_Guide extends Template_Web_Core {
     }
 
     public function action_card() {
+    	$this->template->site->title = "Card Augment List";
+    	$this->template->site->description = "RebuildEQ features a custom set of cards that fit as augments, and this page lists all of them.";
+    	$this->template->site->image = "http://rebuildeq.com/images/cards/card.png";
     	$maxId = 100143;
 		$dbItems = DB::select('id, name, lore')->from('items')->where('id', '>', 100099)->where('id', '<', $maxId)->as_object()->execute()->as_array();
 		
