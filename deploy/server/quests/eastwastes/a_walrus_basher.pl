8f18b3e3bd465b0557a89a26d405a67fea792e12
Xackery <xackery@gmail.com>
2016-10-03 23:16:19 -0700

    more edits for walrus script

--- deploy/server/quests/eastwastes/a_walrus_basher.pl
+++ deploy/server/quests/eastwastes/a_walrus_basher.pl
@@ -1,11 +1,5 @@
-my $i = 0;
-
 sub EVENT_SAY {
 	if($text=~/hail/i) {
-		$i++;
-		$entity_list->GetMobID($mobid)->CastToNPC()->SpellEffect($i);
-		quest::say($i);
-
   		if (int(rand(20)) == 1) {
   			quest::say("Me needs to [ " . quest::saylink("enchantz") . " ] traps for walrus...");			
   			return;
@@ -14,19 +8,49 @@ sub EVENT_SAY {
   			return;
   		}    
   	}
+
+  	if ($text=~/walrus/i) {
+  		quest::say("da walrus r yummy. Giants like dem too. We eats them lots, you should too. It's good!");
+  		return;
+  	}
   	
   	if ($text=~/enchantz/i) {
-  		quest::say("yes, we enchantz.. clams R yummy for clams wid it. We enchantz [ " . quest::saylink("udder stuff") . " ] too..");
+  		quest::say("yes, we enchantz.. enchanted clams R yummy for trap walrus wid it. We enchantz [ " . quest::saylink("udder stuff") . " ] too..");
   		return;
   	}
 
   	if ($text=~/udder stuff/i || $text=~/other stuff/i) {
-  		quest::say("u know, like green things. clams. shrimp.. clams.. elfies.. orange things.. cowmeat.. caimen.. fuds.. mm.. me hungry now. Me miss [ " . quest::saylink("Oggok") . " ] some times.");
+  		quest::say("u know, like green things. clams. shrimp.. elfies.. orange things.. cowmeat.. caimen.. clams.. fuds.. mm.. me hungry now. Me miss [ " . quest::saylink("Oggok") . " ] some times.");
   		return;
   	}
 
-  	if ($text=~/Oggok/i) {
-  		quest::say("Oggok is da bashers home. We bash gud, we bash all night long. But, on second thoughts, tis a silly place.");
+  	if ($text=~/oggok/i) {
+  		quest::say("Oggok is da [ ". quest::saylink("bashers") . " ] home. We bash gud, we bash all night long. But, on second thoughts, tis a silly place.");
+  		return;
+  	}
+
+  	if ($text=~/bashers/i) {
+  		quest::say("Bashers r da best! Nobody stronger than they. we be favorite of [" . quest::saylink("Rallos Zek") . " ], and be da great warrior ever. More stronger than all.");
+  		return;
+  	}
+
+  	if ($text=~/rallos zek/i) {
+  		quest::say("Rallos Zek is strong. u know, he talks about his creaturez a lot. He likes to tell us chapter stories and chapters about stuffs. We calls it the [ " . quest::saylink("Doctrine") . " ].");
+  		return;
+  	}
+
+  	if ($text=~/doctrine/i) {
+  		quest::say("Doctrine, u know.. oh wait.. uh oh... I was not to tell secrets.. but, it's ok, da first chapter is wid us. All Us Ogres keep it secret. Shh. You don't know this though.");
+  		return;
+  	}
+
+  	if ($text=~/know/i) {
+  		quest::say("You know nothing, we know everything, hur hur.. be rallos zek, be strong.");
+  		return;
+  	}
+
+  	if ($race=~/ogre/i) {
+  		quest::say("Hey! You ogres too! Come hunt, we kill lots of walrus. It's fun. You bash. They smash. Weeee!");
   		return;
   	}
 }
