7d5621da98ec1e9e8c36a06a65d7c022604f19dc
Xackery <xackery@gmail.com>
2016-10-06 12:10:48 -0700

    nola no longer does old discordeq logic

--- deploy/server/quests/ecommons/Nola_Z-Ret.pl
+++ deploy/server/quests/ecommons/Nola_Z-Ret.pl
@@ -14,29 +14,3 @@ sub EVENT_SAY {
 		quest::say("Yes, the winds! You may not feel it at first but you're bathed in it. When Shadowrest was cracked open it washed Norrath in a miasma of memories. These ether winds form particularly strong in places that have deep history. This is what we attune to when we bind, we reach into your essence and find the core string at the base of your mortal coil. With a delicate touch and the right pluck we can attune you to that miasma.");
 	}
  }
-
-my $lastId = 0;
-
-sub EVENT_SPAWN {
-    #Get last ID
-    $connect = plugin::LoadMysql();
-    $query = "SELECT `id` FROM qs_player_speech ORDER BY `id` DESC LIMIT 1";
-    $query_handle = $connect->prepare($query);
-    $query_handle->execute();
-    while (@row = $query_handle->fetchrow_array()){
-        $lastId = $row[0];
-    }
-      quest::settimer("discord", 1);
-}
-
-sub EVENT_TIMER {
-      $connect = plugin::LoadMysql();
-    $query = "SELECT `from`, `message`, `id` FROM qs_player_speech WHERE `id` > ? AND `type` = 5 AND `to` = '!discord' LIMIT 1";
-    $query_handle = $connect->prepare($query);
-    $query_handle->execute($lastId);
-    while (@row = $query_handle->fetchrow_array()){
-        quest::we(260, $row[0]." says from discord, '".$row[1]."'");
-        $lastId = $row[2];
-    }
-    return
-}
