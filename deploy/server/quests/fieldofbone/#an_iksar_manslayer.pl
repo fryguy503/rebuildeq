561759295d2570cd1a2467d52ea82f85e460fb39
Jon Packard <jonpackard@users.noreply.github.com>
2017-01-11 16:52:01 -0500

    Corrected error in comment. No code changes.
    I can't seem to get the lesser spirit to spawn after killing "an iksar manslayer". Comparing to other methods to spawn lesser spirit in other zones.

--- deploy/server/quests/fieldofbone/#an_iksar_manslayer.pl
+++ deploy/server/quests/fieldofbone/#an_iksar_manslayer.pl
@@ -1,4 +1,4 @@
-# This is to spawn a lesser spirit upon an_iksar_marauder's death for Shaman epic 1.0
+# This is to spawn a lesser spirit upon an_iksar_manslayer's death for Shaman epic 1.0
 
 sub EVENT_DEATH_COMPLETE {
   my $x = $npc->GetX();
