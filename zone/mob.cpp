9abf759dfba3a82af94fc363e5517b72c6a5d5a7
Joshua Packard <thegrandpackard@gmail.com>
2017-01-12 21:26:50 -0800

    Disabled showing items during #mystats if the NPC is charmed

--- zone/mob.cpp
+++ zone/mob.cpp
@@ -1492,7 +1492,9 @@ void Mob::ShowStats(Client* client)
 				spawngroupid = n->respawn2->SpawnGroupID();
 			client->Message(0, "  NPCID: %u  SpawnGroupID: %u Grid: %i LootTable: %u FactionID: %i SpellsID: %u ", GetNPCTypeID(),spawngroupid, n->GetGrid(), n->GetLoottableID(), n->GetNPCFactionID(), n->GetNPCSpellsID());
 			client->Message(0, "  Accuracy: %i MerchantID: %i EmoteID: %i Runspeed: %.3f Walkspeed: %.3f", n->GetAccuracyRating(), n->MerchantType, n->GetEmoteID(), static_cast<float>(0.025f * n->GetRunspeed()), static_cast<float>(0.025f * n->GetWalkspeed()));
-			n->QueryLoot(client);
+			
+			if(!n->CastToMob()->IsCharmed())
+				n->QueryLoot(client);
 		}
 		if (IsAIControlled()) {
 			client->Message(0, "  AggroRange: %1.0f  AssistRange: %1.0f", GetAggroRange(), GetAssistRange());
