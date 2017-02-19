360522958bb1046c7e6b37fe1aa0ee4aecc2f11f
Joshua Packard <thegrandpackard@gmail.com>
2017-02-19 13:18:19 -0800

    Update 20170218_Decyaing_Skeleton_Faction.sql

--- deploy/sql/20170218_Decyaing_Skeleton_Faction.sql
+++ deploy/sql/20170218_Decyaing_Skeleton_Faction.sql
@@ -1,2 +1,2 @@
 # Change all decaying skeletons to KoS
-UPDATE npc_types SET npc_faction_id = 0 WHERE name LIKE 'a_decaying_skeleton';
+UPDATE npc_types SET npc_faction_id = 79 WHERE name LIKE 'a_decaying_skeleton';
