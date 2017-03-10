d96a4ea296ab54b85a4aa0dca75ee76fe67c8ab8
Joshua Packard <thegrandpackard@gmail.com>
2017-03-09 19:08:01 -0800

    Unscribe and unmem Rod of Mystical Transvergence and Mass Rod of Mystical Transvergence

---
+++ deploy/sql/20170309_Small_Modulation_Shard.sql
@@ -0,0 +1,3 @@
+# Unscribe and unmem Rod of Mystical Transvergence and Mass Rod of Mystical Transvergence
+DELETE FROM character_memmed_spells WHERE spell_id IN (2538,3188);
+DELETE FROM character_spells WHERE spell_id IN (2538,3188);
