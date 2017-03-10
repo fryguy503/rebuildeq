95e0d7ae58ad4d7778371368038d37d7029126ac
Joshua Packard <thegrandpackard@gmail.com>
2017-03-09 19:18:20 -0800

    Unscribe and unmem Manfiest Elements
    Part of the already committed Mage Epic Unscribe branch.

---
+++ deploy/sql/20170309_Manifest_Elements.sql
@@ -0,0 +1,3 @@
+# Unscribe and unmem Manfiest Elements
+DELETE FROM character_spells WHERE character_spells.spell_id = 1936;
+DELETE FROM character_memmed_spells WHERE character_memmed_spells.spell_id = 1936;
