7954c3eb0cb4a624be62861feeb832efe272cec4
Xackery <xackery@gmail.com>
2017-06-01 07:51:56 -0700

    added docker tweaks.

--- build.bat
+++ build.bat
@@ -14,11 +14,10 @@ copy bin\* deploy\server\
 echo Building docker-compose...
 docker-compose build
 
-echo Running shared memory...
-docker run -v %cd%\deploy\server:/eqemu -it eqemu /eqemu/shared_memory
-
 if not exist "deploy\server\eqemu_config.xml" echo Copying eqemu_config.xml... && copy "docker\eqemu\eqemu_config.xml" "deploy\server"
+if not exist "deploy\server\shared" echo Making shared directory... && mkdir deploy\server\shared
 
-docker-compose run eqemu shared_memory
+echo Running shared memory...
+docker-compose run eqemu /eqemu/shared_memory
 
 echo Build completed. Run docker-compose up to inject DB data, or run the start.sh script to begin the environment.
