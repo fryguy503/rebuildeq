3c651bc41cc472f865e5f5c2826c3c2112217bef
jonpackard <jon@packard.tech>
2017-06-04 00:33:19 -0700

    Added fix for bard mana song boost perk

--- zone/bonuses.cpp
+++ zone/bonuses.cpp
@@ -1745,8 +1745,8 @@ void Mob::ApplySpellsBonuses(uint16 spell_id, uint8 casterlevel, StatBonuses *ne
 			case SE_ManaRegen_v2:
 			case SE_CurrentMana:
 			{
-				if (IsClient()) {
-					rank = CastToClient()->GetBuildRank(BARD, RB_BRD_CASSINDRASCHORUS);
+				if (caster) {
+					rank = caster->GetBuildRank(BARD, RB_BRD_CASSINDRASCHORUS);
 					if (rank > 0) {
 						effect_value += (rank * 0.1 * effect_value);
 					}
