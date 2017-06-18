cf6f94fec7dd6e6896565964c79ffa5de830a6e9
Xackery <xackery@gmail.com>
2017-06-17 21:11:54 -0700

    added a new quest line.

--- deploy/server/quests/thurgadina/Saramor.pl
+++ deploy/server/quests/thurgadina/Saramor.pl
@@ -1,3 +1,29 @@
+sub EVENT_SAY {
+	if ($text=~/special/i) {
+        quest::say("Find me a pure frozen core and hand it to me, we'll see from there.");
+        return;
+    }    
+    if ($text=~/smile/i) {
+        quest::say("I'm smiling at you. I wanted to tell you about ".quest::saylink("something special")." I have to offer.");
+        return;
+    }    
+    quest::emote("winks at you with a smile.");
+}
+
+sub EVENT_ITEM {
+  if (plugin::check_handin(\%itemcount, 100085 => 4)) { #hand a pure frozen core
+    quest::emote("looks left and right, then takes the pure frozen core from your hands.")
+    quest::say("Nobody saw this transaction, do you hear me?.");
+    quest::summonitem(100086);
+    
+    # Factions: +Coldain, +Dain Frostreaver IV, -Kromrif, -Kromzek
+    quest::faction(49, 30);
+    quest::faction(67, 30);
+    quest::faction(188, -30);
+    quest::faction(189, -30);
+  }
+}
+
 sub EVENT_SIGNAL {
 	if($signal==1) {
 		quest::say("Blasphemer!");
