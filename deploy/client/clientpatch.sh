795cea87d9ccd0753a64ecec2bb16f272494ee3c
Xackery Xtal <xackery@gmail.com>
2017-06-19 15:50:08 -0700

    added cleaner support for directory creation

--- deploy/client/clientpatch.sh
+++ deploy/client/clientpatch.sh
@@ -4,6 +4,20 @@ set -e
 #used to deploy a client patch
 cd rof
 ./filelistbuilder
-cp -R * ../patch/
+
+if [ ! -d ../patch ]; then
+   echo "Making patch directory..."
+   mkdir ../patch
+fi
+
+if [ ! -d ../patch/rof ]; then
+   echo "Making patch/rof directory..."
+   mkdir ../patch/rof
+fi
+
+cp -R * ../patch/rof/
+
 rm ../patch/filelistbuilder
+rm ../patch/filelistbuilder.yml
 rm ../patch/delete.txt
+echo "Complete."
