c804475f3378030b1ddd00134508cee5acff593a
Ubuntu <ubuntu@rebuildeq.com>
2017-11-13 04:25:38 +0000

    Added clientpatch.sh modifications

--- client_patch/clientpatch.sh
+++ client_patch/clientpatch.sh
@@ -4,7 +4,7 @@ set -e
 go run filelistbuilder.go
 
 rm -rf ../deploy/patch/*
-cp rof ../deploy/patch/
+cp rof ../deploy/patch/ -r
 cp filelist_rof.yml ../deploy/patch/
 cp patch.zip ../deploy/patch/
 cp eqemupatcher.exe ../deploy/patch/
