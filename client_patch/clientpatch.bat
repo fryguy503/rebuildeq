623809087f825e218dd30c68bf8199d123acfa3f
Xackery <xackery@gmail.com>
2017-11-12 20:15:06 -0800

    clientpatch fixed for server run

--- client_patch/clientpatch.bat
+++ client_patch/clientpatch.bat
@@ -5,11 +5,12 @@ go run filelistbuilder.go
 rd /s /q %~dp0..\deploy\patch\rof\
 xcopy rof\* %~dp0..\deploy\patch\rof\ /e /s /h /d
 echo Copying filelist_rof.yml
-copy filelist_rof.yml ..\deploy\patch\rof\ /d
+copy filelist_rof.yml ..\deploy\patch\ /d
 echo Copying patch.zip
 copy patch.zip ..\deploy\patch\ /d
 echo Copying eqemupatcher.exe
-copy "%~dp0\eqemupatcher\EQEmu Patcher\bin\Release\eqemupatcher.exe" %~dp0..\deploy\patch\ /d
+copy "%~dp0\eqemupatcher\EQEmu Patcher\bin\Release\eqemupatcher.exe" . /d
+
 echo Cleaning up lingering local files...
 del patch.zip
 del filelist_rof.yml
---
+++ client_patch/clientpatch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+go run filelistbuilder.go
+
+rm -rf ../deploy/patch/*
+cp rof ../deploy/patch/
+cp filelist_rof.yml ../deploy/patch/
+cp patch.zip ../deploy/patch/
+cp eqemupatcher.exe ../deploy/patch/
+rm patch.zip
+rm filelist_rof.yml
