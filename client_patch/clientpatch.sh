5a8c22f92e18ed00dc67c110c5234c62da50de22
Xackery <xackery@gmail.com>
2017-11-17 09:07:31 -0800

    fixed clientpatch on server to use RebuildEQ.exe

--- client_patch/clientpatch.sh
+++ client_patch/clientpatch.sh
@@ -7,5 +7,5 @@ rm -rf ../deploy/patch/*
 cp rof ../deploy/patch/ -r
 cp filelist_rof.yml ../deploy/patch/rof/
 cp patch.zip ../deploy/patch/
-cp eqemupatcher.exe ../deploy/patch/
+cp RebuildEQ.exe ../deploy/patch/
 rm patch.zip
