302e06bb588432e65cf318f47346e474e9984429
Ceyx <crice67@gmail.com>
2017-12-02 20:53:26 -0800

    Faydedar fix

--- zone/npc.cpp
+++ zone/npc.cpp
@@ -2750,7 +2750,7 @@ std::vector<SpecialLoot_Struct> NPC::SpecialLoot(bool card_only) {
 
 	for (auto &&i : SpecialLoots) {
 		Log(Logs::General, Logs::Debug, "ID: %i, String: %s, Race: %i", i.item_id, i.name.c_str(), GetRace());
-		if (i.item_id == 100100 && (GetRace() == 195 /*Zland*/ || GetRace() == 19 /*Trak*/ || GetRace() == 49 /*Sev*/ || GetNPCTypeID() == 96073 /*Fey*/ || GetRace() == 184 /*aary*/)) loot_list.push_back(i); //Dragon Card
+		if (i.item_id == 100100 && (GetRace() == 195 /*Zland*/ || GetRace() == 19 /*Trak*/ || GetRace() == 49 /*Sev*/ || GetNPCTypeID() == 96073 /*Fey*/ || GetRace() == 184 /*aary*/ || GetNPCTypeID() == 96089 /*Faydedar*/)) loot_list.push_back(i); //Dragon Card
 		if (i.item_id == 100101 && GetBodyType() == BT_Insect) loot_list.push_back(i); //Insect Card
 		if (i.item_id == 100102 && (GetBodyType() == BT_Animal || GetRace() == 36 || GetRace() == 156 || GetRace() == 415)) loot_list.push_back(i); //Animal Card
 		if (i.item_id == 100103 && GetBodyType() == BT_Construct) loot_list.push_back(i); //Construct Card
