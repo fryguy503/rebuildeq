c86afdf6ad2ab8c59197232882348cfe6ce30a61
Xackery Xtal <xackery@gmail.com>
2017-12-08 06:58:10 -0800

    added npc and item

--- go/web/server/item.go
+++ go/web/server/item.go
@@ -1,13 +1,66 @@
 package server
 
 import (
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-type Item struct {
-}
-
 func GetItem(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	vars := mux.Vars(r)
+
+	item, resp, err := api.ItemApi.GetItem(nil, vars["id"])
+	if err != nil {
+		//TODO: Handle errors more gracefully
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Failed", err.Error())
+		return
+	}
+	if resp.StatusCode != 200 {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Invalid status code", err.Error())
+		return
+	}
+
+	tmp := getTemplate("")
+	if tmp == nil {
+
+		newTmp, tErr := loadTemplate(nil, "body", "item.tpl")
+		if tErr != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Println("failed to load template", tErr.Error())
+			return
+		}
+		newTmp, tErr = loadTemplate(newTmp, "navmenu", "navmenu.tpl")
+		if tErr != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Println("failed to load template", tErr.Error())
+			return
+		}
+		newTmp, tErr = loadTemplate(newTmp, "root", "root.tpl")
+		if tErr != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Println("failed to load template", tErr.Error())
+			return
+		}
+		setTemplate("item", newTmp)
+		tmp = newTmp
+	}
+	type Content struct {
+		Site Site
+		Item interface{}
+	}
+	content := Content{
+		Site: site,
+		Item: item,
+	}
+
 	w.WriteHeader(http.StatusOK)
+	if err = tmp.Execute(w, content); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Template rendering error", err.Error())
+		return
+	}
+	return
 }
--- go/web/server/npc.go
+++ go/web/server/npc.go
@@ -1,13 +1,66 @@
 package server
 
 import (
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-type NPC struct {
-}
-
 func GetNPC(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	vars := mux.Vars(r)
+
+	npc, resp, err := api.NPCApi.GetNPC(nil, vars["id"])
+	if err != nil {
+		//TODO: Handle errors more gracefully
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Failed", err.Error())
+		return
+	}
+	if resp.StatusCode != 200 {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Invalid status code", err.Error())
+		return
+	}
+
+	tmp := getTemplate("")
+	if tmp == nil {
+
+		newTmp, tErr := loadTemplate(nil, "body", "npc.tpl")
+		if tErr != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Println("failed to load template", tErr.Error())
+			return
+		}
+		newTmp, tErr = loadTemplate(newTmp, "navmenu", "navmenu.tpl")
+		if tErr != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Println("failed to load template", tErr.Error())
+			return
+		}
+		newTmp, tErr = loadTemplate(newTmp, "root", "root.tpl")
+		if tErr != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Println("failed to load template", tErr.Error())
+			return
+		}
+		setTemplate("npc", newTmp)
+		tmp = newTmp
+	}
+	type Content struct {
+		Site Site
+		Npc  interface{}
+	}
+	content := Content{
+		Site: site,
+		Npc:  npc,
+	}
+
 	w.WriteHeader(http.StatusOK)
+	if err = tmp.Execute(w, content); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Template rendering error", err.Error())
+		return
+	}
+	return
 }
