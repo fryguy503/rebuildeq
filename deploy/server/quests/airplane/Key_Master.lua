df309560328b76e6f21ed0ab7121224c9d6ce877
Ceyx <crice67@gmail.com>
2017-12-21 20:14:29 -0800

    keeper of souls fix

--- deploy/server/quests/airplane/Key_Master.lua
+++ deploy/server/quests/airplane/Key_Master.lua
@@ -17,7 +17,6 @@ function event_signal(e)
 		-- eq.set_timer("13",300000); what is this referred to?
 		eq.set_global("keeper","1",3,"H2");
 		-- supposed to be 60-85 minute timer
-		eq.set_timer("87", 10);
 	end
 end
 
