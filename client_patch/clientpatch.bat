ea5910c49e2817618f0f44e3dd83f7264f30cd50
Xackery <xackery@gmail.com>
2018-02-04 03:42:15 -0800

    Changed patcher to use github repo

--- client_patch/clientpatch.bat
+++ client_patch/clientpatch.bat
@@ -12,7 +12,8 @@ echo Copying RebuildEQ.exe
 copy "%~dp0\eqemupatcher\EQEmu Patcher\bin\Release\eqemupatcher.exe" . /d
 del RebuildEQ.exe
 rename eqemupatcher.exe RebuildEQ.exe
-
+echo Copying to rebuildeq repo
+xcopy rof\* %~dp0..\..\..\rebuildeq\rebuildeq\
 echo Cleaning up lingering local files...
 del patch.zip
 del filelist_rof.yml
--- client_patch/eqemupatcher/EQEmu Patcher/MainForm.cs
+++ client_patch/eqemupatcher/EQEmu Patcher/MainForm.cs
@@ -23,7 +23,7 @@ namespace EQEmu_Patcher
          * 
          ****/
         public static string serverName = "Rebuild EQ";
-        public static string filelistUrl = "http://rebuildeq.com/patch/";
+        public static string filelistUrl = "https://raw.githubusercontent.com/rebuildeq/rebuildeq/master/";
         public static bool defaultAutoPlay = false; //When a user runs this first time, what should Autoplay be set to?
         public static bool defaultAutoPatch = false; //When a user runs this first time, what should Autopatch be set to?
 
