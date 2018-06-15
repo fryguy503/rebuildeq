7945f6bcb8e66a6260cffab642233d91a867586e
Jon Packard <jon@packard.tech>
2018-06-15 01:49:15 -0400

    One more change to mob.cpp and it works now!

--- zone/mob.cpp
+++ zone/mob.cpp
@@ -7941,11 +7941,10 @@ int Mob::ModifyManaUsage(int mana_cost, uint16 spell_id, Mob* spell_target, bool
 	rank = GetBuildRank(DRUID, RB_DRU_TELEPORTBIND);
 	if (rank > 0 && spell_id == 5953) {
 		// 85% Mana at Rank 1, minus 15% per rank: 85,70,55,40,25
-		int mana_cost = floor(GetMaxMana());
 		int redux = floor(GetMaxMana() * rank * 0.15f);
 		
 		BuildEcho(StringFormat("Teleport Bind %i reduced mana cost by %i.", rank, redux));
-		mana_cost -= redux;
+		mana_cost = (GetMaxMana() - redux);
 	}
 	
 	rank = GetBuildRank(NECROMANCER, RB_NEC_LIFEBURN);
