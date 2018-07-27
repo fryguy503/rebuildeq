c0416f75ffdbf836505a803db10e98d3da83dd25
Trust <fryguy503@gmail.com>
2018-07-26 20:01:20 -0400

    Fix spell push
    This now uses the spell data correctly, still need to write a system for
    NPCs though
    
    SE_TossUp needs to be implemented to, it just damages NPCs only
    
    EQEmu
    
    1b849059419cae9dcb22e538bfad189cb05b849e

--- zone/spells.cpp
+++ zone/spells.cpp
@@ -2889,106 +2889,72 @@ void Mob::BardPulse(uint16 spell_id, Mob *caster) {
 	int buffs_i;
 	int buff_count = GetMaxTotalSlots();
 	for (buffs_i = 0; buffs_i < buff_count; buffs_i++) {
-		if(buffs[buffs_i].spellid != spell_id)
+		if (buffs[buffs_i].spellid != spell_id)
 			continue;
-		if(buffs[buffs_i].casterid != caster->GetID()) {
-			Log(Logs::Detail, Logs::Spells, "Bard Pulse for %d: found buff from caster %d and we are pulsing for %d... are there two bards playing the same song???", spell_id, buffs[buffs_i].casterid, caster->GetID());
+		if (buffs[buffs_i].casterid != caster->GetID()) {
+			Log(Logs::Detail, Logs::Spells,
+				"Bard Pulse for %d: found buff from caster %d and we are pulsing for %d... are there two bards playing the same song???",
+				spell_id, buffs[buffs_i].casterid, caster->GetID());
 			return;
 		}
 		//extend the spell if it will expire before the next pulse
-		if(buffs[buffs_i].ticsremaining <= 3) {
+		if (buffs[buffs_i].ticsremaining <= 3) {
 			buffs[buffs_i].ticsremaining += 3;
-			Log(Logs::Detail, Logs::Spells, "Bard Song Pulse %d: extending duration in slot %d to %d tics", spell_id, buffs_i, buffs[buffs_i].ticsremaining);
+			Log(Logs::Detail, Logs::Spells, "Bard Song Pulse %d: extending duration in slot %d to %d tics", spell_id,
+				buffs_i, buffs[buffs_i].ticsremaining);
 		}
 
 		//should we send this buff update to the client... seems like it would
 		//be a lot of traffic for no reason...
 //this may be the wrong packet...
-		if(IsClient()) {
+		if (IsClient()) {
 			auto packet = new EQApplicationPacket(OP_Action, sizeof(Action_Struct));
 
-			Action_Struct* action = (Action_Struct*) packet->pBuffer;
+			Action_Struct *action = (Action_Struct *) packet->pBuffer;
 			action->source = caster->GetID();
 			action->target = GetID();
 			action->spell = spell_id;
+			action->force = spells[spell_id].pushback;
 			action->hit_heading = GetHeading();
+			action->hit_pitch = spells[spell_id].pushup;
 			action->instrument_mod = caster->GetInstrumentMod(spell_id);
 			action->effect_flag = 0;
 			action->spell_level = action->level = buffs[buffs_i].casterlevel;
 			action->type = DamageTypeSpell;
-			entity_list.QueueCloseClients(this, packet, false, RuleI(Range, SongMessages), 0, true, IsClient() ? FilterPCSpells : FilterNPCSpells);
+			entity_list.QueueCloseClients(this, packet, false, RuleI(Range, SongMessages), 0, true,
+										  IsClient() ? FilterPCSpells : FilterNPCSpells);
 
 			action->effect_flag = 4;
 
-			if(IsEffectInSpell(spell_id, SE_TossUp))
-			{
-				action->effect_flag = 0;
-			}
-			else if(spells[spell_id].pushback > 0 || spells[spell_id].pushup > 0)
-			{
-				if(IsClient())
-				{
-					if(!IsBuffSpell(spell_id))
-					{
+			if (spells[spell_id].pushback > 0 || spells[spell_id].pushup > 0) {
+				if (IsClient()) {
+					if (!IsBuffSpell(spell_id)) {
 						CastToClient()->SetKnockBackExemption(true);
-
-						action->effect_flag = 0;
-						auto outapp_push = new EQApplicationPacket(
-						    OP_ClientUpdate, sizeof(PlayerPositionUpdateServer_Struct));
-						PlayerPositionUpdateServer_Struct* spu = (PlayerPositionUpdateServer_Struct*)outapp_push->pBuffer;
-
-						double look_heading = caster->CalculateHeadingToTarget(GetX(), GetY());
-						look_heading /= 256;
-						look_heading *= 360;
-						if(look_heading > 360)
-							look_heading -= 360;
-
-						//x and y are crossed mkay
-						double new_x = spells[spell_id].pushback * sin(double(look_heading * 3.141592 / 180.0));
-						double new_y = spells[spell_id].pushback * cos(double(look_heading * 3.141592 / 180.0));
-
-						spu->spawn_id	= GetID();
-						spu->x_pos		= FloatToEQ19(GetX());
-						spu->y_pos		= FloatToEQ19(GetY());
-						spu->z_pos		= FloatToEQ19(GetZ());
-						spu->delta_x	= FloatToEQ13(new_x);
-						spu->delta_y	= FloatToEQ13(new_y);
-						spu->delta_z	= FloatToEQ13(spells[spell_id].pushup);
-						spu->heading	= FloatToEQ12(GetHeading());
-						spu->padding0002	=0;
-						spu->padding0006	=7;
-						spu->padding0014	=0x7f;
-						spu->padding0018	=0x5df27;
-						spu->animation = 0;
-						spu->delta_heading = FloatToEQ10(0);
-						outapp_push->priority = 6;
-						entity_list.QueueClients(this, outapp_push, true);
-						CastToClient()->FastQueuePacket(&outapp_push);
 					}
 				}
 			}
 
-			if(IsClient() && IsEffectInSpell(spell_id, SE_ShadowStep))
-			{
+			if (IsClient() && IsEffectInSpell(spell_id, SE_ShadowStep)) {
 				CastToClient()->SetShadowStepExemption(true);
 			}
 
-			if(!IsEffectInSpell(spell_id, SE_BindAffinity))
-			{
+			if (!IsEffectInSpell(spell_id, SE_BindAffinity)) {
 				CastToClient()->QueuePacket(packet);
 			}
 
 			auto message_packet = new EQApplicationPacket(OP_Damage, sizeof(CombatDamage_Struct));
-			CombatDamage_Struct *cd = (CombatDamage_Struct *)message_packet->pBuffer;
+			CombatDamage_Struct *cd = (CombatDamage_Struct *) message_packet->pBuffer;
 			cd->target = action->target;
 			cd->source = action->source;
 			cd->type = DamageTypeSpell;
 			cd->spellid = action->spell;
+			cd->force = action->force;
 			cd->hit_heading = action->hit_heading;
+			cd->hit_pitch = action->hit_pitch;
 			cd->damage = 0;
-			if(!IsEffectInSpell(spell_id, SE_BindAffinity))
-			{
-				entity_list.QueueCloseClients(this, message_packet, false, RuleI(Range, SongMessages), 0, true, IsClient() ? FilterPCSpells : FilterNPCSpells);
+			if (!IsEffectInSpell(spell_id, SE_BindAffinity)) {
+				entity_list.QueueCloseClients(this, message_packet, false, RuleI(Range, SongMessages), 0, true,
+											  IsClient() ? FilterPCSpells : FilterNPCSpells);
 			}
 			safe_delete(message_packet);
 			safe_delete(packet);
@@ -4073,7 +4039,9 @@ bool Mob::SpellOnTarget(uint16 spell_id, Mob *spelltar, bool reflect, bool use_r
 	action->level = caster_level;	// caster level, for animation only
 	action->type = 231;	// 231 means a spell
 	action->spell = spell_id;
+	action->force = spells[spell_id].pushback;
 	action->hit_heading = GetHeading();
+	action->hit_pitch = spells[spell_id].pushup;
 	action->instrument_mod = GetInstrumentMod(spell_id);
 	action->effect_flag = 0;
 
@@ -4537,64 +4505,22 @@ bool Mob::SpellOnTarget(uint16 spell_id, Mob *spelltar, bool reflect, bool use_r
 	// the complete sequence is 2 actions and 1 damage message
 	action->effect_flag = 0x04;	// this is a success flag
 
-	if(IsEffectInSpell(spell_id, SE_TossUp))
-	{
-		action->effect_flag = 0;
-	}
-	else if(spells[spell_id].pushback > 0 || spells[spell_id].pushup > 0)
-	{
-		if(spelltar->IsClient())
-		{
-			if(!IsBuffSpell(spell_id))
-			{
+	if(spells[spell_id].pushback > 0 || spells[spell_id].pushup > 0) {
+		if (spelltar->IsClient()) {
+			if (!IsBuffSpell(spell_id)) {
 				spelltar->CastToClient()->SetKnockBackExemption(true);
-
-				action->effect_flag = 0;
-				auto outapp_push =
-				    new EQApplicationPacket(OP_ClientUpdate, sizeof(PlayerPositionUpdateServer_Struct));
-				PlayerPositionUpdateServer_Struct* spu = (PlayerPositionUpdateServer_Struct*)outapp_push->pBuffer;
-
-				double look_heading = CalculateHeadingToTarget(spelltar->GetX(), spelltar->GetY());
-				look_heading /= 256;
-				look_heading *= 360;
-				if(look_heading > 360)
-					look_heading -= 360;
-
-				//x and y are crossed mkay
-				double new_x = spells[spell_id].pushback * sin(double(look_heading * 3.141592 / 180.0));
-				double new_y = spells[spell_id].pushback * cos(double(look_heading * 3.141592 / 180.0));
-
-				spu->spawn_id	= spelltar->GetID();
-				spu->x_pos		= FloatToEQ19(spelltar->GetX());
-				spu->y_pos		= FloatToEQ19(spelltar->GetY());
-				spu->z_pos		= FloatToEQ19(spelltar->GetZ());
-				spu->delta_x	= FloatToEQ13(new_x);
-				spu->delta_y	= FloatToEQ13(new_y);
-				spu->delta_z	= FloatToEQ13(spells[spell_id].pushup);
-				spu->heading	= FloatToEQ12(spelltar->GetHeading());
-				spu->padding0002	=0;
-				spu->padding0006	=7;
-				spu->padding0014	=0x7f;
-				spu->padding0018	=0x5df27;
-				spu->animation = 0;
-				spu->delta_heading = FloatToEQ10(0);
-				outapp_push->priority = 6;
-				entity_list.QueueClients(this, outapp_push, true);
-				spelltar->CastToClient()->FastQueuePacket(&outapp_push);
 			}
 		}
 	}
 
-	if(spelltar->IsClient() && IsEffectInSpell(spell_id, SE_ShadowStep))
-	{
+	if(spelltar->IsClient() && IsEffectInSpell(spell_id, SE_ShadowStep)) {
 		spelltar->CastToClient()->SetShadowStepExemption(true);
 	}
 
-	if(!IsEffectInSpell(spell_id, SE_BindAffinity))
-	{
-		if(spelltar != this && spelltar->IsClient())	// send to target
+	if(!IsEffectInSpell(spell_id, SE_BindAffinity)) {
+		if (spelltar != this && spelltar->IsClient())    // send to target
 			spelltar->CastToClient()->QueuePacket(action_packet);
-		if(IsClient())	// send to caster
+		if (IsClient())    // send to caster
 			CastToClient()->QueuePacket(action_packet);
 	}
 	// send to people in the area, ignoring caster and target
@@ -4609,17 +4535,19 @@ bool Mob::SpellOnTarget(uint16 spell_id, Mob *spelltar, bool reflect, bool use_r
 	cd->source = action->source;
 	cd->type = action->type;
 	cd->spellid = action->spell;
+	cd->force = action->force;
 	cd->hit_heading = action->hit_heading;
+	cd->hit_pitch = action->hit_pitch;
 	cd->damage = 0;
-	if(!IsEffectInSpell(spell_id, SE_BindAffinity)){
+	if(!IsEffectInSpell(spell_id, SE_BindAffinity)) {
 		entity_list.QueueCloseClients(
-			spelltar, /* Sender */
-			message_packet, /* Packet */
-			false, /* Ignore Sender */
-			RuleI(Range, SpellMessages), 
-			0, /* Skip this mob */
-			true, /* Packet ACK */
-			(spelltar->IsClient() ? FilterPCSpells : FilterNPCSpells) /* Message Filter Type: (8 or 9) */
+				spelltar, /* Sender */
+				message_packet, /* Packet */
+				false, /* Ignore Sender */
+				RuleI(Range, SpellMessages),
+				0, /* Skip this mob */
+				true, /* Packet ACK */
+				(spelltar->IsClient() ? FilterPCSpells : FilterNPCSpells) /* Message Filter Type: (8 or 9) */
 		);
 	}
 	safe_delete(action_packet);
