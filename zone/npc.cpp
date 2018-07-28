79b7590ed2b57f79ec0c72bf239b63c1296d6e93
Trust <fryguy503@gmail.com>
2018-07-27 22:19:08 -0400

    Pull out forced movement from push to it's own function
    Had to pull this out so we could process it before mez/stun
    
    EQEmu
    
    876335bb54a5ef0c9f7954a0c4b1d1bc51e27e5c

--- zone/npc.cpp
+++ zone/npc.cpp
@@ -771,6 +771,11 @@ bool NPC::Process()
 		reface_timer->Disable();
 	}
 
+	// needs to be done before mez and stun
+	if (ForcedMovement)
+		ProcessForcedMovement();
+
+
 	if (IsMezzed())
 		return true;
 
