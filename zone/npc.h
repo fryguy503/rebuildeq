d4b1dc69098481618037633d2fdef2453d4778fe
Trust <fryguy503@gmail.com>
2018-08-04 10:08:03 -0400

    Add #grid show comand to display a targeted NPCs grid points visually
    EQEmu
    
    0d094754db25c5d4374061285e420773a61b5bc3

--- zone/npc.h
+++ zone/npc.h
@@ -117,6 +117,8 @@ public:
 
 	virtual ~NPC();
 
+	static NPC *SpawnGridNodeNPC(std::string name, const glm::vec4 &position, uint32 grid_id, uint32 grid_number, uint32 pause);
+
 	//abstract virtual function implementations requird by base abstract class
 	virtual bool Death(Mob* killerMob, int32 damage, uint16 spell_id, EQEmu::skills::SkillType attack_skill);
 	virtual void Damage(Mob* from, int32 damage, uint16 spell_id, EQEmu::skills::SkillType attack_skill, bool avoidable = true, int8 buffslot = -1, bool iBuffTic = false, eSpecialAttacks special = eSpecialAttacks::None);
