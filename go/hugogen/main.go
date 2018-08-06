e270bad3a98c9257a7c647a4fc0a026b34282745
Xackery Xtal <xackery@gmail.com>
2018-08-06 04:15:50 -0700

    Added NPC Generation support

--- go/hugogen/main.go
+++ go/hugogen/main.go
@@ -9,10 +9,11 @@ import (
 	"github.com/jmoiron/sqlx"
 	"github.com/xackery/eqemuconfig"
 	"github.com/xackery/goeq/item"
+	"github.com/xackery/goeq/npc"
 )
 
 func main() {
-	outPath := "../../web/hugo/content/item/"
+
 	config, err := eqemuconfig.GetConfig()
 	if err != nil {
 		fmt.Println("Error getting config:", err.Error())
@@ -26,8 +27,24 @@ func main() {
 		fmt.Println("Error connecting to DB:", err.Error())
 		return
 	}
+	outPath := "../../web/hugo/content/"
+	total := 150
+	err = genItems(db, outPath+"item/", total)
+	err = genNPCs(db, outPath+"npc/", total)
 
-	rows, err := db.Queryx(fmt.Sprintf("SELECT * FROM items LIMIT 20"))
+}
+
+func connectDB(config *eqemuconfig.Config) (db *sqlx.DB, err error) {
+	//Connect to DB
+	db, err = sqlx.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=true", config.Database.Username, config.Database.Password, config.Database.Host, config.Database.Port, config.Database.Db))
+	if err != nil {
+		return
+	}
+	return
+}
+
+func genItems(db *sqlx.DB, outPath string, total int) (err error) {
+	rows, err := db.Queryx(fmt.Sprintf("SELECT * FROM items LIMIT %d", total))
 	if err != nil {
 		fmt.Println("Error deleting merchanlist", err.Error())
 		return
@@ -56,13 +73,50 @@ func main() {
 			continue
 		}
 	}
+	return
 }
 
-func connectDB(config *eqemuconfig.Config) (db *sqlx.DB, err error) {
-	//Connect to DB
-	db, err = sqlx.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=true", config.Database.Username, config.Database.Password, config.Database.Host, config.Database.Port, config.Database.Db))
+func genNPCs(db *sqlx.DB, outPath string, total int) (err error) {
+	count := 0
+	rows, err := db.Queryx(fmt.Sprintf("SELECT id, race, name, class FROM npc_types LIMIT %d", total))
 	if err != nil {
+		fmt.Println("Error deleting merchanlist", err.Error())
 		return
 	}
+
+	for rows.Next() {
+		n := &npc.NpcTypes{}
+		err = rows.StructScan(&n)
+		if err != nil {
+			fmt.Println("Error scanning", err.Error())
+			return
+		}
+
+		//NPC MUST have a spawn point to be listed.. (we will write another detection for quest npcs)
+		var val int
+		db.Get(&val, "SELECT count(spawngroupID) FROM spawnentry WHERE npcid = ?", n.Id.Int64)
+		fmt.Println(val, n.Id.Int64)
+		if val < 1 {
+			continue
+		}
+		count++
+
+		outStr := "+++\n"
+		outStr += fmt.Sprintf("note = \"This file was auto generated. DO NOT EDIT\"\n")
+		outStr += fmt.Sprintf("description = \"%d\"\n", n.Race)
+		outStr += fmt.Sprintf("id = \"%d\"\n", n.Id.Int64)
+		outStr += fmt.Sprintf("icon = \"%d\"\n", 1)
+		outStr += fmt.Sprintf("tags = [%d]\n", n.Id.Int64)
+		outStr += fmt.Sprintf("title = \"%s\"\n", n.Name)
+		outStr += fmt.Sprintf("class = %d\n", n.Class)
+		outStr += "+++\n"
+		err = ioutil.WriteFile(fmt.Sprintf("%s/%s-%d.md", outPath, slug.MakeLang(n.Name, "en"), n.Id.Int64), []byte(outStr), 0644)
+		if err != nil {
+			fmt.Println("Error writing file:", err.Error())
+			continue
+		}
+	}
+
+	fmt.Println(count, "total npcs")
 	return
 }
