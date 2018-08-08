04661f194223d6803341ce0031465b876436d240
Xackery Xtal <xackery@gmail.com>
2018-08-08 02:53:28 -0700

    Added tree point spending support

--- web/src/build/build.ts
+++ web/src/build/build.ts
@@ -6,13 +6,15 @@ export class SkillConfig {
 export class Skill {
 	private element:Element;
 	private spentPoints:number = 0;
-	public _build: Build;
-	private index:number;
+	private _build: Build;
+	private _index:number = 0;
+	private _points:number = 0;
 
 	constructor(params) {
 		this.build = params.build;
 		this.element = params.element;
-		
+
+		if (this.element == null) return;
 		this.index = parseInt(this.element.getAttribute("index"))
 		this.element.addEventListener("mousedown", (e: MouseEvent) => { this.onMouseDown(e); }, false)
 	}
@@ -25,20 +27,20 @@ export class Skill {
 		this._build = build;
 	}
 
-	public GetSpentPoints():number {
-		return this.spentPoints;
+	get index(): number {
+		return this._index;
 	}
 
-	public SetSpentPoints(value:number) {
-		this.spentPoints = value;
+	set index(index:number) {
+		this._index = index;
 	}
 
-	public AddPoint(value:number) {
-		this.spentPoints += value;
+	get points(): number {
+		return this._points;
 	}
 
-	public GetIndex() {
-		return this.index;
+	set points(points:number) {		
+		this._points = points;
 	}
 
 	private onMouseDown(event:MouseEvent) {
@@ -57,49 +59,155 @@ export class Build {
 	private isLocked:boolean = false;
 	private level:number = 60;
 	private spentPoints:number = 0;
-	private skills:Map<number, Skill> = new Map();
+	private _skills:Map<number, Skill> = new Map();
 
-	public SetSkills(skills:Map<number, Skill>) {
-		this.skills = skills;
+	
+	get skills(): Map<number, Skill> {
+		return this._skills;
+	}
+
+	set skills(skills:Map<number, Skill>) {
+		this._skills = skills;
+	}
+
+	public LoadHash(hash:string) {
+		//build a temporary skill list
+		let skills = new Map<number, Skill>();
+		for (let i = 0; i < 32; i++) {
+			let value = 0;
+			if (hash.length < i) {
+				value = 0;
+			} else {
+				value = parseInt(hash.substr(0,1))
+			}
+			//Make dummy skill
+			let skill = new Skill({});			
+			skill.points = value;
+		}
+		let reason = this.isValid(skills);
+		if (reason != "") {
+			console.log("failed to parse: "+reason);
+			return;
+		}
+		for (let i = 0; i < 32; i++) {
+			let skill = this.skills.get(i);
+			if (skill == null) continue;
+			let newSkill = skills.get(i);
+			if (newSkill == null) continue;
+			skill.points = newSkill.points;
+		}
+		console.log("loaded hash, result:"+this.getHash());
 	}
 
 	public SpendPoint(skill:Skill, value:number) {
+		console.log("spending point at "+skill.index+" to value "+value);
 		if (this.isLocked) return;
-		if (skill.GetSpentPoints() >= 5) {
+		if (skill.points >= 5) {
 			console.log("can't spend point, skill already max");
 			return;
 		}
 		if (this.spentPoints >= this.level) {
-			console.log("can't spend point, all points spent for level")
-			return
+			console.log("can't spend point, all points spent for level");
+			return;
 		}
-		//check getTreeTotal
 
+		//attempt for validation
+		skill.points += 1;
+		this.spentPoints++;
+		let reason = this.isValid(this.skills);
+		if (reason != "") {
+			skill.points -= 1;
+			this.spentPoints--;
+			console.log("can't spend points: "+reason);
+			return;
+		}
+		
 		//request ajax
 
-		this.spentPoints++;
-		skill.AddPoint(1);
 		
-		console.log(this.getBuild());
+		console.log(this.getHash());
 	}
 
-	private getTreeTotal(skill:Skill):number {
-		let spent = 0;
-
-		return spent;
-	}
-
-	private getBuild():string {
-		let build = "";
+	private getHash():string {
+		let hash = "";
 		for (let i = 0; i < 32; i++) {
-			let skill = this.skills.get(i);
-			if (skill == null) {
-				build += "0";
-				continue;
-			}
-			build += skill.GetSpentPoints();
+			let value = this.getSkillPoints(i);
+			hash += value;
 		}
-		return build;
+		return hash;
+	}
+
+	//is a skilltree valid?
+	private isValid(skills:Map<number, Skill>):string {
+		if (skills == null) return "internal error, null skills used";
+		
+		let disabled:number[] = [1,3,5,7,9,11,12,14,15,17,19,21,23,25,27,29,30,32,33,35,37,39,41,43,45,47,48,50,51,53];
+		for (let i = 0; i < disabled.length; i++) {			
+			if (this.getSkillPoints(disabled[i]) > 0) return "this skill is not valid";
+		}
+
+		//each tree is an abbreviation, l = left tree. the number is the row. layer 1 = top row
+		let l1 = this.getSkillPoints(0) + this.getSkillPoints(1) + this.getSkillPoints(2)
+		let m1 = this.getSkillPoints(18) + this.getSkillPoints(19) + this.getSkillPoints(20)
+		let r1 = this.getSkillPoints(36) + this.getSkillPoints(37) + this.getSkillPoints(38)
+
+		let l2 = this.getSkillPoints(3) + this.getSkillPoints(4) + this.getSkillPoints(5)
+		let m2 = this.getSkillPoints(21) + this.getSkillPoints(22) + this.getSkillPoints(23)
+		let r2 = this.getSkillPoints(39) + this.getSkillPoints(40) + this.getSkillPoints(41)
+
+		if ((l1 < 5 && l2 > 0) || (m1 < 5 && m2 > 0) || (r1 < 5 && r2 > 0)) return "at least 5 points must be spent in the prior row";
+
+		l2 += l1;
+		m2 += m1;
+		r2 += r1;
+
+		let l3 = this.getSkillPoints(6) + this.getSkillPoints(7) + this.getSkillPoints(8)
+		let m3 = this.getSkillPoints(24) + this.getSkillPoints(25) + this.getSkillPoints(26)
+		let r3 = this.getSkillPoints(42) + this.getSkillPoints(43) + this.getSkillPoints(44)
+
+		if ((l2 < 5 && l3 > 0) || (m2 < 5 && m3 > 0) || (r2 < 5 && r3 > 0)) return "at least 5 points must be spent in the prior row";
+
+		l3 += l2;
+		m3 += m2;
+		r3 += r2;
+
+		let l4 = this.getSkillPoints(9) + this.getSkillPoints(10) + this.getSkillPoints(11)
+		let m4 = this.getSkillPoints(27) + this.getSkillPoints(28) + this.getSkillPoints(29)
+		let r4 = this.getSkillPoints(45) + this.getSkillPoints(46) + this.getSkillPoints(47)
+
+		if ((l3 < 5 && l4 > 0) || (m3 < 5 && m4 > 0) || (r3 < 5 && r4 > 0)) return "at least 5 points must be spent in the prior row";
+
+		l4 += l3;
+		m4 += m3;
+		r4 += r3;
+
+		let l5 = this.getSkillPoints(12) + this.getSkillPoints(13) + this.getSkillPoints(14)
+		let m5 = this.getSkillPoints(30) + this.getSkillPoints(31) + this.getSkillPoints(32)
+		let r5 = this.getSkillPoints(48) + this.getSkillPoints(49) + this.getSkillPoints(50)
+
+		if ((l4 < 5 && l5 > 0) || (m4 < 5 && m5 > 0) || (r4 < 5 && r5 > 0)) return "at least 5 points must be spent in the prior row";
+
+		l5 += l4;
+		m5 += m4;
+		r5 += r4;
+
+		let l6 = this.getSkillPoints(15) + this.getSkillPoints(16) + this.getSkillPoints(17)
+		let m6 = this.getSkillPoints(33) + this.getSkillPoints(34) + this.getSkillPoints(35)
+		let r6 = this.getSkillPoints(51) + this.getSkillPoints(52) + this.getSkillPoints(53)
+
+		if ((l5 < 5 && l6 > 0) || (m5 < 5 && m6 > 0) || (r5 < 5 && r6 > 0)) return "at least 5 points must be spent in the prior row";
+		
+		l6 += l5;
+		m6 += m5;
+		r6 += r5;
+		
+		return "";
+	}
+
+	private getSkillPoints(index:number):number {
+		let skill = this.skills.get(index);
+		if (skill == null) return 0;
+		return skill.points;
 	}
 }
 
@@ -116,7 +224,8 @@ window.onload = () => {
 	let skills = new Map<number, Skill>();
 	for (let skillElement of skillElements) {
 		let skill = new Skill({element: skillElement, build: build});		
-		skills.set(skill.GetIndex(), skill);
+		skills.set(skill.index, skill);
 	}
-	build.SetSkills(skills);
+	build.skills = skills;
+	console.log("loaded");
 }
