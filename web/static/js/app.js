69fb35f9297c9130f48b4fe227e5ed4f5adab371
Xackery Xtal <xackery@gmail.com>
2018-08-12 17:58:27 -0700

    added misc missing items for web

--- web/static/js/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const build_1 = require("./build");
-const ui_1 = require("./ui");
-window.onload = () => {
-    build_1.OnWindowLoadBuild();
-    ui_1.OnWindowLoadUI();
-};
