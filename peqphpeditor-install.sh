e2b6619272e4964b751063bec8e9b45fc390ed51
Jon Packard <jonpackard@users.noreply.github.com>
2018-11-10 13:59:57 -0500

    Update confirmation message

--- peqphpeditor-install.sh
+++ peqphpeditor-install.sh
@@ -4,7 +4,7 @@ curl -o peqphpeditor.zip -L https://github.com/ProjectEQ/peqphpeditor/archive/ma
 && rm -rfv peqphpeditor-master \
 && cp docker/web/peqphpeditor-config.php deploy/web/html/peqphpeditor/config.php \
 && cd deploy/web/html/peqphpeditor \
-&& if [ ! -e logs ]; then  mkdir logs; fi \
+&& if [ ! -e logs ]; then mkdir logs; fi \
 && find . -type d -exec chmod 0755 {} + && find . -type f -exec chmod 0644 {} + \
-&& echo Peqphpeditor installation successful.
-
+&& echo "Peqphpeditor installation successful." \
+"Open http://[server]/peqphpadmin and log in with admin / password"
