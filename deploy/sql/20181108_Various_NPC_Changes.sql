46411d4f280eda071ebb71a6df2f9e2a10255242
Jon Packard <jonpackard@users.noreply.github.com>
2018-11-09 22:09:21 -0500

    Remove unneeded parenthesis on surname for Lieutenant_Bythgar

--- deploy/sql/20181108_Various_NPC_Changes.sql
+++ deploy/sql/20181108_Various_NPC_Changes.sql
@@ -3,4 +3,4 @@
 UPDATE `npc_types` SET `special_abilities` = NULL WHERE (`id` = '120010');
 
 #Issue 1736 - Add (Armor Quest) last name to Lieutenant_Bythgar
-UPDATE npc_types SET lastname="(Armor Quest)" WHERE id=115071; -- whovian (10-Nov-18  2:48:18)
+UPDATE npc_types SET lastname="Armor Quest" WHERE id=115071; -- whovian (10-Nov-18  2:48:18)
