08495b4fe76ab1db78742771b73c5a8f4c9f7b50
Ubuntu <ubuntu@ip-172-26-11-242.us-west-2.compute.internal>
2018-11-10 20:53:51 +0000

    Cleaned up a few things in install.sh

--- install.sh
+++ install.sh
@@ -20,13 +20,18 @@ cp bin/* deploy/server/
 echo Building docker-compose...
 docker-compose build
 
-[ -e deploy/server/eqemu_config.xml ] || echo Copying eqemu_config.xml... && cp docker/eqemu/eqemu_config.xml deploy/server/
-[ -e deploy/web/application/config/database.php ] || echo Copying database.php... && cp docker/web/database.php deploy/web/application/config/
-[ -e deploy/web/application/cache ] || echo Making web cache directory... && mkdir deploy/web/application/cache
-[ -e deploy/web/application/logs ] || echo Making web logs directory... && mkdir deploy/web/application/logs
-[ -e deploy/server/shared ] || echo Making shared directory... && mkdir deploy/server/shared
+[ ! -e docker/db ] && echo Making docker/db directory... && mkdir docker/db
+echo "Making the database directory writable. Enter sudo password if promtped."
+sudo chmod 777 docker/db
+[ ! -e deploy/server/logs ] && echo Making deploy/server/logs directory... && mkdir deploy/server/logs
+[ ! -e deploy/server/eqemu_config.xml ] && echo Copying eqemu_config.xml... && cp docker/eqemu/eqemu_config.xml deploy/server/
+[ ! -e deploy/web/application/config/database.php ] && echo Copying database.php... && cp docker/web/database.php deploy/web/application/config/
+[ ! -e deploy/web/application/cache ] && echo Making web cache directory... && mkdir deploy/web/application/cache
+[ ! -e deploy/web/application/logs ] && echo Making web logs directory... && mkdir deploy/web/application/logs
+[ ! -e deploy/server/shared ] && echo Making shared directory... && mkdir deploy/server/shared
 
 chmod 777 deploy/web/application/logs && echo Making web logs directory writable...
+chmod 777 deploy/web/application/cache && echo Making web cache directory writable...
 
 echo Running shared memory...
 docker-compose run eqemu /eqemu/shared_memory
